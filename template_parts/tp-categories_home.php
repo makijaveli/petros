e15a17e7c41f74cb4505f23ea8671d2f844740a8
mwebrs <marcomilic@mwe.rs>
2017-05-18 12:07:17 +0200

    Izmene

--- template_parts/tp-categories_home.php
+++ template_parts/tp-categories_home.php
@@ -17,22 +17,23 @@
       <div class="card" style="background-image: url('<?php bloginfo('template_directory'); ?>/images/card1.png')">
             <div class="overlay-cards"></div>
 
-
             <div class="cardtitle">
               <?php $category_link = get_category_link( 4 ); ?>
               <h2><a href="<?php echo esc_url( $category_link ); ?>"><?php echo get_cat_name(4);?></a></h2>
-
             </div>
 
             <div class="cardinfo">
 
-              <?php
+              <?php the_field('discover_zagreb'); ?>
+
+              <?php /*
                 $subcategories = get_categories('&child_of=4&hide_empty'); // List subcategories of category '4' (even the ones with no posts in them)
                 echo '<ul>';
                 foreach ($subcategories as $subcategory) {
                   echo sprintf('<li><a href="%s">%s</a></li>', get_category_link($subcategory->term_id), apply_filters('get_term', $subcategory->name));
                 }
                 echo '</ul>';
+                */
               ?>
 
           </div>
@@ -50,14 +51,16 @@
           </div>
 
           <div class="cardinfo">
-
-              <?php
+              <?php the_field('day_trips_from_zagreb'); ?>
+              
+              <?php /*
                 $subcategories = get_categories('&child_of=5&hide_empty'); // List subcategories of category '4' (even the ones with no posts in them)
                 echo '<ul>';
                 foreach ($subcategories as $subcategory) {
                   echo sprintf('<li><a href="%s">%s</a></li>', get_category_link($subcategory->term_id), apply_filters('get_term', $subcategory->name));
                 }
                 echo '</ul>';
+                */
               ?>
 
           </div>
@@ -74,14 +77,16 @@
           </div>
 
           <div class="cardinfo">
-
-            <?php
+            <?php the_field('private_tours'); ?>
+            
+            <?php /*
               $subcategories = get_categories('&child_of=6&hide_empty'); // List subcategories of category '4' (even the ones with no posts in them)
               echo '<ul>';
               foreach ($subcategories as $subcategory) {
                 echo sprintf('<li><a href="%s">%s</a></li>', get_category_link($subcategory->term_id), apply_filters('get_term', $subcategory->name));
               }
               echo '</ul>';
+              */
             ?>
 
           </div>
@@ -102,14 +107,15 @@
           </div>
 
           <div class="cardinfo">
-
-            <?php
+            <?php the_field('petros_apartments'); ?>
+            <?php /*
               $subcategories = get_categories('&child_of=7&hide_empty'); // List subcategories of category '4' (even the ones with no posts in them)
               echo '<ul>';
               foreach ($subcategories as $subcategory) {
                 echo sprintf('<li><a href="%s">%s</a></li>', get_category_link($subcategory->term_id), apply_filters('get_term', $subcategory->name));
               }
               echo '</ul>';
+              */
             ?>
 
           </div>

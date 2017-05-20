99a31eacea1c7e2a1251ddd2e14f48a8afa75aeb
mwebrs <marcomilic@mwe.rs>
2017-05-20 11:04:54 +0200

    Izmene

--- template_parts/tp-followus.php
+++ template_parts/tp-followus.php
@@ -5,7 +5,7 @@
     <h1><?php _e( 'Follow us', 'petros' ); ?></h1>
 
       <?php if(ICL_LANGUAGE_CODE=='en') { ?>
-        For more informaration regarding our tours and other news you can connect, <br>
+        <p>For more informaration regarding our tours and other news you can connect, <br>
         follow and have a conversation with us on our official social media pages.</p>
       <?php } else { ?>
         <p>Za više informacija o našim turama i vestima, možete nas zapratiti i <br>
@@ -14,10 +14,10 @@
 
       <div class="followsocial">
         <ul>
-          <li><a href="https://www.facebook.com/pages/Petros-Travel-Agency-putni%C4%8Dka-agencija/147790758658182"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.instagram.com/explore/tags/hrvatska/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.linkedin.com/company/petros-travel-agency?trk=company_name"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.linkedin.com/company/petros-travel-agency?trk=company_name"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.facebook.com/pages/Petros-Travel-Agency-putni%C4%8Dka-agencija/147790758658182" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.instagram.com/explore/tags/hrvatska/?hl=en" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.linkedin.com/company/petros-travel-agency?trk=company_name"><i class="fa fa-linkedin-square" aria-hidden="true" target="_blank"></i></a></li>
+          <li><a href="https://www.tripadvisor.com/Attraction_Review-g294454-d6786745-Reviews-Petros-Zagreb_Central_Croatia.html" target="_blank"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a></li>
         </ul>
       </div>
     </div>
